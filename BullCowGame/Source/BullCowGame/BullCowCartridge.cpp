cfa5af0eb55c42beb055d04a4006490ecadd047f
Max Gomes <maxgomes92@live.com>
2020-09-04 20:38:51 +0200

    Add win/loose

--- BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,21 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    HiddenWord = TEXT("stockholm");
+    
+    PrintLine(TEXT("Welcome to the game!"));
+    PrintLine(TEXT("Guess the word"));
+    PrintLine(TEXT("Press tab to begin..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+    if (Input == HiddenWord) {
+        PrintLine(TEXT("Win"));
+    } else {
+        PrintLine(TEXT("Loose"));
+    }
 }
